f6b8d3c111a3132572fac3c5935b7fbac6a6325a
fiomercado <mercadoangelfiona@gmail.com>
2025-06-16 15:32:31 +0800

    Add files via upload

--- src/model/FormData.java
+++ src/model/FormData.java
@@ -1,4 +1,279 @@
 package model;
 
 public class FormData {
+    // Personal Information
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String suffix;
+    private String birthMonth;
+    private String birthDay;
+    private String birthYear;
+    private String age;
+    private String sex;
+    private String civilStatus;
+    private String religion;
+    private String citizenship;
+    private String address;
+    private String permanentAddress;
+    private String houseType;
+    private String income;
+    private String contact;
+    private String email;
+    private String cellphone;
+    private String gwa;
+    private String degreeProgram;
+    private String schoolName;
+    private String schoolAddress;
+    private String schoolEmail;
+    private String schoolId;
+
+    // Parent Information
+    private String parentType;
+    private String parentFirstName;
+    private String parentMiddleName;
+    private String parentLastName;
+    private String parentSuffix;
+    private String parentBirthMonth;
+    private String parentBirthDay;
+    private String parentBirthYear;
+    private String parentAge;
+    private String parentSex;
+    private String parentCitizenship;
+    private String parentCivilStatus;
+    private String parentOccupation;
+    private String parentEmail;
+    private String parentMonthlyIncome;
+    private String parentEducationalAttainment;
+
+    // Legacy parent fields (keeping for compatibility)
+    private String parentName;
+    private String parentContact;
+    private String parentAddress;
+    private String parentRelation;
+
+    // Child Information
+    private String childFirstName;
+    private String childMiddleName;
+    private String childLastName;
+    private String childSuffix;
+    private String childBirthMonth;
+    private String childBirthDay;
+    private String childBirthYear;
+    private String childAge;
+    private String childSex;
+    private String childGradeLevel;
+    private String childSchoolName;
+    private String childSchoolAddress;
+
+    private String childName;
+    private String childGrade;
+    private String childSchool;
+
+
+    public FormData() {}
+
+    // Personal Information Getters and Setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getMiddleName() { return middleName; }
+    public void setMiddleName(String middleName) { this.middleName = middleName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getSuffix() { return suffix; }
+    public void setSuffix(String suffix) { this.suffix = suffix; }
+
+    public String getBirthMonth() { return birthMonth; }
+    public void setBirthMonth(String birthMonth) { this.birthMonth = birthMonth; }
+
+    public String getBirthDay() { return birthDay; }
+    public void setBirthDay(String birthDay) { this.birthDay = birthDay; }
+
+    public String getBirthYear() { return birthYear; }
+    public void setBirthYear(String birthYear) { this.birthYear = birthYear; }
+
+    public String getAge() { return age; }
+    public void setAge(String age) { this.age = age; }
+
+    public String getSex() { return sex; }
+    public void setSex(String sex) { this.sex = sex; }
+
+    public String getCivilStatus() { return civilStatus; }
+    public void setCivilStatus(String civilStatus) { this.civilStatus = civilStatus; }
+
+    public String getReligion() { return religion; }
+    public void setReligion(String religion) { this.religion = religion; }
+
+    public String getCitizenship() { return citizenship; }
+    public void setCitizenship(String citizenship) { this.citizenship = citizenship; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPermanentAddress() { return permanentAddress; }
+    public void setPermanentAddress(String permanentAddress) { this.permanentAddress = permanentAddress; }
+
+    public String getHouseType() { return houseType; }
+    public void setHouseType(String houseType) { this.houseType = houseType; }
+
+    public String getIncome() { return income; }
+    public void setIncome(String income) { this.income = income; }
+
+    public String getContact() { return contact; }
+    public void setContact(String contact) { this.contact = contact; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getCellphone() { return cellphone; }
+    public void setCellphone(String cellphone) { this.cellphone = cellphone; }
+
+    public String getGwa() { return gwa; }
+    public void setGwa(String gwa) { this.gwa = gwa; }
+
+    public String getDegreeProgram() { return degreeProgram; }
+    public void setDegreeProgram(String degreeProgram) { this.degreeProgram = degreeProgram; }
+
+    public String getSchoolName() { return schoolName; }
+    public void setSchoolName(String schoolName) { this.schoolName = schoolName; }
+
+    public String getSchoolAddress() { return schoolAddress; }
+    public void setSchoolAddress(String schoolAddress) { this.schoolAddress = schoolAddress; }
+
+    public String getSchoolEmail() { return schoolEmail; }
+    public void setSchoolEmail(String schoolEmail) { this.schoolEmail = schoolEmail; }
+
+    public String getSchoolId() { return schoolId; }
+    public void setSchoolId(String schoolId) { this.schoolId = schoolId; }
+
+    // Parent Information Getters and Setters
+    public String getParentType() { return parentType; }
+    public void setParentType(String parentType) { this.parentType = parentType; }
+
+    public String getParentFirstName() { return parentFirstName; }
+    public void setParentFirstName(String parentFirstName) { this.parentFirstName = parentFirstName; }
+
+    public String getParentMiddleName() { return parentMiddleName; }
+    public void setParentMiddleName(String parentMiddleName) { this.parentMiddleName = parentMiddleName; }
+
+    public String getParentLastName() { return parentLastName; }
+    public void setParentLastName(String parentLastName) { this.parentLastName = parentLastName; }
+
+    public String getParentSuffix() { return parentSuffix; }
+    public void setParentSuffix(String parentSuffix) { this.parentSuffix = parentSuffix; }
+
+    public String getParentBirthMonth() { return parentBirthMonth; }
+    public void setParentBirthMonth(String parentBirthMonth) { this.parentBirthMonth = parentBirthMonth; }
+
+    public String getParentBirthDay() { return parentBirthDay; }
+    public void setParentBirthDay(String parentBirthDay) { this.parentBirthDay = parentBirthDay; }
+
+    public String getParentBirthYear() { return parentBirthYear; }
+    public void setParentBirthYear(String parentBirthYear) { this.parentBirthYear = parentBirthYear; }
+
+    public String getParentAge() { return parentAge; }
+    public void setParentAge(String parentAge) { this.parentAge = parentAge; }
+
+    public String getParentSex() { return parentSex; }
+    public void setParentSex(String parentSex) { this.parentSex = parentSex; }
+
+    public String getParentCitizenship() { return parentCitizenship; }
+    public void setParentCitizenship(String parentCitizenship) { this.parentCitizenship = parentCitizenship; }
+
+    public String getParentCivilStatus() { return parentCivilStatus; }
+    public void setParentCivilStatus(String parentCivilStatus) { this.parentCivilStatus = parentCivilStatus; }
+
+    public String getParentOccupation() { return parentOccupation; }
+    public void setParentOccupation(String parentOccupation) { this.parentOccupation = parentOccupation; }
+
+    public String getParentEmail() { return parentEmail; }
+    public void setParentEmail(String parentEmail) { this.parentEmail = parentEmail; }
+
+    public String getParentMonthlyIncome() { return parentMonthlyIncome; }
+    public void setParentMonthlyIncome(String parentMonthlyIncome) { this.parentMonthlyIncome = parentMonthlyIncome; }
+
+    public String getParentEducationalAttainment() { return parentEducationalAttainment; }
+    public void setParentEducationalAttainment(String parentEducationalAttainment) { this.parentEducationalAttainment = parentEducationalAttainment; }
+
+    // Legacy parent getters and setters
+    public String getParentName() { return parentName; }
+    public void setParentName(String parentName) { this.parentName = parentName; }
+
+    public String getParentContact() { return parentContact; }
+    public void setParentContact(String parentContact) { this.parentContact = parentContact; }
+
+    public String getParentAddress() { return parentAddress; }
+    public void setParentAddress(String parentAddress) { this.parentAddress = parentAddress; }
+
+    public String getParentRelation() { return parentRelation; }
+    public void setParentRelation(String parentRelation) { this.parentRelation = parentRelation; }
+
+    // Child Information Getters and Setters
+    public String getChildFirstName() { return childFirstName; }
+    public void setChildFirstName(String childFirstName) { this.childFirstName = childFirstName; }
+
+    public String getChildMiddleName() { return childMiddleName; }
+    public void setChildMiddleName(String childMiddleName) { this.childMiddleName = childMiddleName; }
+
+    public String getChildLastName() { return childLastName; }
+    public void setChildLastName(String childLastName) { this.childLastName = childLastName; }
+
+    public String getChildSuffix() { return childSuffix; }
+    public void setChildSuffix(String childSuffix) { this.childSuffix = childSuffix; }
+
+    public String getChildBirthMonth() { return childBirthMonth; }
+    public void setChildBirthMonth(String childBirthMonth) { this.childBirthMonth = childBirthMonth; }
+
+    public String getChildBirthDay() { return childBirthDay; }
+    public void setChildBirthDay(String childBirthDay) { this.childBirthDay = childBirthDay; }
+
+    public String getChildBirthYear() { return childBirthYear; }
+    public void setChildBirthYear(String childBirthYear) { this.childBirthYear = childBirthYear; }
+
+    public String getChildAge() { return childAge; }
+    public void setChildAge(String childAge) { this.childAge = childAge; }
+
+    public String getChildSex() { return childSex; }
+    public void setChildSex(String childSex) { this.childSex = childSex; }
+
+    public String getChildGradeLevel() { return childGradeLevel; }
+    public void setChildGradeLevel(String childGradeLevel) { this.childGradeLevel = childGradeLevel; }
+
+    public String getChildSchoolName() { return childSchoolName; }
+    public void setChildSchoolName(String childSchoolName) { this.childSchoolName = childSchoolName; }
+
+    public String getChildSchoolAddress() { return childSchoolAddress; }
+    public void setChildSchoolAddress(String childSchoolAddress) { this.childSchoolAddress = childSchoolAddress; }
+
+    // Legacy child getters and setters
+    public String getChildName() { return childName; }
+    public void setChildName(String childName) { this.childName = childName; }
+
+    public String getChildGrade() { return childGrade; }
+    public void setChildGrade(String childGrade) { this.childGrade = childGrade; }
+
+    public String getChildSchool() { return childSchool; }
+    public void setChildSchool(String childSchool) { this.childSchool = childSchool; }
+
+    @Override
+    public String toString() {
+        return "FormData{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", suffix='" + suffix + '\'' +
+                ", email='" + email + '\'' +
+                ", cellphone='" + cellphone + '\'' +
+                ", degreeProgram='" + degreeProgram + '\'' +
+                ", schoolName='" + schoolName + '\'' +
+                ", parentType='" + parentType + '\'' +
+                ", parentFirstName='" + parentFirstName + '\'' +
+                ", parentLastName='" + parentLastName + '\'' +
+                ", childFirstName='" + childFirstName + '\'' +
+                ", childLastName='" + childLastName + '\'' +
+                '}';
+    }
 }
--- src/model/MainApp.java
+++ src/model/MainApp.java
@@ -1,4 +1,281 @@
 package model;
 
-public class MainApp {
+import com.formdev.flatlaf.FlatLightLaf;
+import panels.FillOutFormPanel;
+import panels.FinalizeApplicationPanel;
+import panels.HelpSupportPanel;
+import panels.SignOutPanel;
+import panels.ProfileInfoPanel;
+import panels.AboutUsPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Objects;
+
+public class MainApp extends JFrame {
+
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private FillOutFormPanel formPanel;
+    private FinalizeApplicationPanel finalizePanel;
+
+    private String userName;
+    private String userGmail;
+
+    public MainApp(String userName, String userGmail) {
+        this.userName = userName;
+        this.userGmail = userGmail;
+
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+            UIManager.put("defaultFont", new Font("Century Gothic", Font.PLAIN, 14));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        initUI();
+    }
+
+    public MainApp(String username) {
+    }
+
+    private void initUI() {
+        setTitle("EduRise Scholarship Program");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(900, 600);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(new BorderLayout());
+
+        JPanel whiteBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        whiteBar.setBackground(Color.WHITE);
+        whiteBar.setPreferredSize(new Dimension(900, 45));
+
+        ImageIcon logoIcon = null;
+        try {
+            logoIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/icon/logoremoveprev.png")));
+        } catch (Exception e) {
+            System.err.println("Logo image not found");
+        }
+        if (logoIcon != null) {
+            Image scaledImage = logoIcon.getImage().getScaledInstance(55, 40, Image.SCALE_SMOOTH);
+            JLabel logoLabel = new JLabel(new ImageIcon(scaledImage));
+            whiteBar.add(logoLabel);
+        }
+
+        JPanel statusRow = new JPanel(new BorderLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gp = new GradientPaint(0, 0, new Color(123, 8, 40), getWidth(), 0, new Color(82, 43, 71));
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        statusRow.setPreferredSize(new Dimension(900, 60));
+
+        JLabel title = new JLabel("Welcome, " + userName + "!");
+        title.setFont(new Font("Century Gothic", Font.BOLD, 40));
+        title.setForeground(new Color(232, 178, 59));
+
+        JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        titlePanel.setOpaque(false);
+        titlePanel.add(title);
+
+        JPanel leftStatus = new JPanel();
+        leftStatus.setLayout(new BoxLayout(leftStatus, BoxLayout.Y_AXIS));
+        leftStatus.setOpaque(false);
+        leftStatus.add(titlePanel);
+
+        JButton aboutUsBtn = makeFlatLink("About Us");
+        aboutUsBtn.addActionListener(e -> cardLayout.show(mainPanel, "AboutUs"));
+
+        ImageIcon profileIcon = null;
+        try {
+            profileIcon = new ImageIcon(getClass().getResource("/icon/profile.png"));
+        } catch (Exception e) {
+            System.err.println("Profile icon image not found");
+        }
+        JLabel profileImageLabel = new JLabel();
+        if (profileIcon != null) {
+            Image resizedImg = profileIcon.getImage().getScaledInstance(53, 53, Image.SCALE_SMOOTH);
+            profileImageLabel.setIcon(new ImageIcon(resizedImg));
+        }
+        profileImageLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        profileImageLabel.setToolTipText("View Profile");
+        profileImageLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cardLayout.show(mainPanel, "Profile");
+            }
+        });
+
+        JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        rightPanel.setOpaque(false);
+        rightPanel.add(aboutUsBtn);
+        rightPanel.add(profileImageLabel);
+
+        statusRow.add(leftStatus, BorderLayout.WEST);
+        statusRow.add(rightPanel, BorderLayout.EAST);
+
+        JPanel infoBar = new JPanel(new BorderLayout());
+        infoBar.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
+        infoBar.setBackground(new Color(131, 95, 121));
+
+        JLabel nameLabel = new JLabel("Name: " + userName);
+        JLabel emailLabel = new JLabel("Gmail: " + userGmail);
+        nameLabel.setForeground(Color.WHITE);
+        emailLabel.setForeground(Color.WHITE);
+        infoBar.add(nameLabel, BorderLayout.WEST);
+        infoBar.add(emailLabel, BorderLayout.EAST);
+
+        JPanel dashboardPanel = new JPanel();
+        dashboardPanel.setLayout(new BoxLayout(dashboardPanel, BoxLayout.Y_AXIS));
+        dashboardPanel.setBackground(Color.WHITE);
+
+        JButton btnForm = makeModernButton("Fill-out Form", "/icon/filloutbg.png");
+        JButton btnFinalize = makeModernButton("Finalize Application", "/icon/finalizebg.png");
+        btnForm.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnFinalize.setAlignmentX(Component.CENTER_ALIGNMENT);
+        dashboardPanel.add(Box.createVerticalStrut(30));
+        dashboardPanel.add(btnForm);
+        dashboardPanel.add(Box.createVerticalStrut(20));
+        dashboardPanel.add(btnFinalize);
+
+        JPanel footer = new JPanel(new BorderLayout()) {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                GradientPaint gp = new GradientPaint(0, 0, new Color(123, 8, 40), getWidth(), 0, new Color(82, 43, 71));
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        footer.setPreferredSize(new Dimension(900, 30));
+        JButton help = makeFlatLink("Help & Support");
+        JButton signOut = makeFlatLink("Sign out");
+        footer.add(help, BorderLayout.WEST);
+        footer.add(signOut, BorderLayout.EAST);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+
+        JPanel homePanel = new JPanel(new BorderLayout());
+        homePanel.add(dashboardPanel, BorderLayout.CENTER);
+
+        finalizePanel = new FinalizeApplicationPanel(cardLayout, mainPanel);
+        formPanel = new FillOutFormPanel(cardLayout, mainPanel, formData -> {
+            finalizePanel.setFormData(formData);
+            cardLayout.show(mainPanel, "Finalize");
+        });
+
+        HelpSupportPanel helpPanel = new HelpSupportPanel(cardLayout, mainPanel);
+        SignOutPanel signOutPanel = new SignOutPanel(cardLayout, mainPanel);
+        ProfileInfoPanel profilePanel = new ProfileInfoPanel(cardLayout, mainPanel, userName, userGmail);
+        AboutUsPanel aboutUsPanel = new AboutUsPanel(cardLayout, mainPanel);
+
+        mainPanel.add(homePanel, "Home");
+        mainPanel.add(formPanel, "Form");
+        mainPanel.add(finalizePanel, "Finalize");
+        mainPanel.add(helpPanel, "Help");
+        mainPanel.add(signOutPanel, "SignOut");
+        mainPanel.add(profilePanel, "Profile");
+        mainPanel.add(aboutUsPanel, "AboutUs");
+
+        btnForm.addActionListener(e -> cardLayout.show(mainPanel, "Form"));
+        btnFinalize.addActionListener(e -> {
+            var data = formPanel.collectFormData();
+            if (data != null) {
+                finalizePanel.setFormData(data);
+                cardLayout.show(mainPanel, "Finalize");
+            } else {
+                JOptionPane.showMessageDialog(this, "Please complete the form first.", "Incomplete", JOptionPane.WARNING_MESSAGE);
+                cardLayout.show(mainPanel, "Form");
+            }
+        });
+        help.addActionListener(e -> cardLayout.show(mainPanel, "Help"));
+        setupSignOutButton(signOut);
+
+        JPanel topContainer = new JPanel(new BorderLayout());
+        topContainer.add(whiteBar, BorderLayout.NORTH);
+        topContainer.add(statusRow, BorderLayout.CENTER);
+        topContainer.add(infoBar, BorderLayout.SOUTH);
+
+        add(topContainer, BorderLayout.NORTH);
+        add(mainPanel, BorderLayout.CENTER);
+        add(footer, BorderLayout.SOUTH);
+
+        cardLayout.show(mainPanel, "Home");
+    }
+
+    private void setupSignOutButton(JButton signOutButton) {
+        signOutButton.addActionListener(e -> {
+            dispose();
+            SwingUtilities.invokeLater(() -> new window.LoginWindow().setVisible(true)); // reopen login window
+        });
+    }
+
+    public void showApp() {
+        setVisible(true);
+    }
+
+    private JLabel makeStatusLabel(String text, Color color, boolean bold) {
+        JLabel label = new JLabel(text);
+        label.setForeground(color);
+        label.setFont(new Font("Century Gothic", bold ? Font.BOLD : Font.PLAIN, 12));
+        return label;
+    }
+
+    private JButton makeModernButton(String text, String imagePath) {
+        JButton btn = new JButton(text);
+        btn.setFont(new Font("Century Gothic", Font.BOLD, 18));
+        btn.setBackground(Color.WHITE);
+        btn.setFocusPainted(false);
+        btn.setMaximumSize(new Dimension(280, 45));
+        btn.setHorizontalAlignment(SwingConstants.LEFT); // Align text to the left
+
+        ImageIcon icon = null;
+        try {
+            icon = new ImageIcon(Objects.requireNonNull(getClass().getResource(imagePath)));
+            Image image = icon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH); // Scale image if needed
+            icon = new ImageIcon(image);
+        } catch (Exception e) {
+            System.err.println("Image not found: " + imagePath);
+        }
+
+        if (icon != null) {
+            btn.setIcon(icon);
+            btn.setIconTextGap(10);
+        }
+
+        return btn;
+    }
+
+    private JButton makeFlatLink(String text) {
+        JButton btn = new JButton(text);
+        btn.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+        btn.setForeground(Color.WHITE);
+        btn.setOpaque(false);
+        btn.setContentAreaFilled(false);
+        btn.setBorderPainted(false);
+        btn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        btn.setFocusPainted(false);
+
+        btn.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent evt) {
+                btn.setForeground(new Color(232, 178, 59));
+            }
+
+            public void mouseExited(MouseEvent evt) {
+                btn.setForeground(Color.WHITE);
+            }
+        });
+
+        return btn;
+    }
 }
